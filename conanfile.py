e22aa230abbb7272f0275dc67b491934cf05fd16
smokris <smokris@softpixel.com>
2018-04-02 11:19:03 -0400

    Support building on Linux.

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, tools
+import platform
 
 class OscPackConan(ConanFile):
     name = 'oscpack'
@@ -18,6 +19,11 @@ class OscPackConan(ConanFile):
     exports_sources = '*.patch'
 
     def source(self):
+        if platform.system() == 'Darwin':
+            flags = '-mmacosx-version-min=10.10'
+        elif platform.system() == 'Linux':
+            flags = '-fPIC'
+
         self.run("git clone https://github.com/vuo/oscpack.git")
         with tools.chdir(self.source_dir):
             self.run("git checkout facab13")
@@ -26,10 +32,13 @@ class OscPackConan(ConanFile):
                                   'CXX := %s' % self.deps_cpp_info['llvm'].rootpath + '/bin/clang++')
             tools.replace_in_file('Makefile',
                                   'COPTS  := -Wall -Wextra -O3',
-                                  'COPTS  := -Wall -Wextra -Oz -mmacosx-version-min=10.10 -stdlib=libstdc++')
+                                  'COPTS  := -Wall -Wextra -Oz -stdlib=libstdc++ ' + flags)
             tools.replace_in_file('Makefile',
                                   '	$(CXX) -dynamiclib -Wl,-install_name,$(LIBSONAME) -o $(LIBFILENAME) $(LIBOBJECTS) -lc',
                                   '	$(CXX) -dynamiclib -Wl,-install_name,@rpath/liboscpack.dylib -Oz -mmacosx-version-min=10.10 -stdlib=libstdc++ -o $(LIBFILENAME) $(LIBOBJECTS) -lc')
+            tools.replace_in_file('Makefile',
+                                  '	@ldconfig',
+                                  '	echo skipping ldconfig')
 
         # http://web.archive.org/web/20170102013425/https://code.google.com/archive/p/oscpack/issues/15
         tools.patch(patch_file='udpsocket-get-port.patch', base_path=self.source_dir)
@@ -41,13 +50,22 @@ class OscPackConan(ConanFile):
         tools.mkdir('%s/lib' % self.install_dir)
         with tools.chdir(self.source_dir):
             self.run('make install PREFIX=../%s' % self.install_dir)
-        with tools.chdir(self.install_dir):
-            self.run('rm lib/liboscpack.so')
-            self.run('mv lib/liboscpack.so.1.1.0 lib/liboscpack.dylib')
+
+        with tools.chdir('%s/lib' % self.install_dir):
+            self.run('rm liboscpack.so')
+            if platform.system() == 'Darwin':
+                self.run('mv liboscpack.so.1.1.0 liboscpack.dylib')
+            elif platform.system() == 'Linux':
+                self.run('mv liboscpack.so.1.1.0 liboscpack.so')
 
     def package(self):
+        if platform.system() == 'Darwin':
+            libext = 'dylib'
+        elif platform.system() == 'Linux':
+            libext = 'so'
+
         self.copy('*.h',     src='%s/include' % self.install_dir, dst='include')
-        self.copy('*.dylib', src='%s/lib'     % self.install_dir, dst='lib')
+        self.copy('liboscpack.%s' % libext, src='%s/lib' % self.install_dir, dst='lib')
 
         self.copy('%s.txt' % self.name, src=self.source_dir, dst='license')
 
